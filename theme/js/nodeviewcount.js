2cebc6754297af446b04592ecbf55af9ae079c1a
adci_contributor <ak@adcillc.com>
2018-11-15 10:59:13 +0600

    Issue #2937398 by adci_contributor: IP-Adress

--- theme/js/nodeviewcount.js
+++ theme/js/nodeviewcount.js
@@ -4,17 +4,18 @@
  */
 
 jQuery(document).ready(function ($) {
-  var nodeviewcount_insert_node_view = function (nid, uid) {
+  var nodeviewcount_insert_node_view = function (nid, uid, uip) {
     var nodeviewcount_path = Drupal.settings.nodeviewcount.nodeviewcount_path;
     var nodeviewcount_timestamp = Drupal.settings.nodeviewcount.nodeviewcount_timestamp;
     var nodeviewcount_token = Drupal.settings.nodeviewcount.nodeviewcount_token;
     $.ajax({
       type: 'POST',
-      url: Drupal.settings.basePath + nodeviewcount_path + '/' + nid + '/' + uid + '/' + nodeviewcount_timestamp + '/' + nodeviewcount_token,
+      url: Drupal.settings.basePath + nodeviewcount_path + '/' + nid + '/' + uid + '/' + nodeviewcount_timestamp + '/' + uip +'/'+ nodeviewcount_token,
       dataType: 'json'
     });
   }
   var nodeviewcount_nid = Drupal.settings.nodeviewcount.nodeviewcount_nid;
   var nodeviewcount_uid = Drupal.settings.nodeviewcount.nodeviewcount_uid;
-  nodeviewcount_insert_node_view(nodeviewcount_nid, nodeviewcount_uid);
+  var nodeviewcount_uip = Drupal.settings.nodeviewcount.nodeviewcount_uip;
+  nodeviewcount_insert_node_view(nodeviewcount_nid, nodeviewcount_uid, nodeviewcount_uip);
 });
